aed0f74d4c3209d742e02e248d37f8ca4b7686a6
Jexeones24 <jexeones@gmail.com>
2019-08-10 15:20:44 -0400

    uses css grid and adds fxnality to create correct # of weeks etc

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from 'react';
 import moment from 'moment';
-import './App.css';
-import styled from 'styled-components';
+
+import {
+  AppContainer,
+  Cell,
+  Day,
+  Grid,
+  WeekdayHeader,
+  Header,
+  Week,
+  Weekday,
+} from './styles';
 
 const DAYS_OF_WEEK = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
-
-const Day = ({ children }) => (
-  <Cell>
-    {children}
-  </Cell>
-);
 
 const Navigation = ({ icon, onClick }) => (
   <button onClick={onClick}>
@@ -17,28 +20,61 @@ const Navigation = ({ icon, onClick }) => (
   </button>
 );
 
-const Header = ({ date, handleNavigationClick }) => (
-  <HeaderContainer>
-    <Navigation icon={'<'} onClick={() => handleNavigationClick('back')} />
-    <div>{date.format('MMMM')}</div>
-    <div>{date.format('YYYY')}</div>
-    <Navigation icon={'>'} onClick={() => handleNavigationClick('forward')} />
-  </HeaderContainer>
+const DaysOfWeek = () => (
+  <WeekdayHeader>
+    {DAYS_OF_WEEK.map((day, i) => <Weekday key={i}>{day}</Weekday>)}
+  </WeekdayHeader>
 );
 
-const DaysOfWeek = () => DAYS_OF_WEEK.map((day, i) => <Day key={i}>{day}</Day>);
-const EmptyCells = ({ count }) => [...Array(count)].map((_, i) => <Cell key={i} />);
-const Days = ({ count }) => [...Array(count)].map((_, i) => <Day key={i}>{i + 1}</Day>);
 
-const Cells = ({ numberOfDaysInMonth, startIdx }) => {
+const Weeks = ({ numberOfDaysInMonth, startIdx }) => {
   const maxCellCount = (startIdx >= 5 && numberOfDaysInMonth === 31) ? 42 : 35;
   const remainingCellCount = maxCellCount - (startIdx + numberOfDaysInMonth);
+  const numberOfWeeks = maxCellCount / 7;
+
+  const firstWeekEmptyCells = [...Array(startIdx)].fill('hello');
+  const firstWeekDays = [...Array(7 - startIdx)].map((_, i) => i + 1);
+  const firstWeek = firstWeekEmptyCells.concat(firstWeekDays);
+
+  const getNextStartIdx = previousWeek => previousWeek[previousWeek.length - 1];
+
+  const generateRemainingWeeks = (startIdx) => {
+    const numberOfRemainingWeeks = numberOfWeeks - 1;
+    let nextIdx = startIdx;
+    let weeks = [];
+
+    for (var i = 1; i <= numberOfRemainingWeeks; i++) {
+      let week = [];
+
+      for (var j = 1; j <= 7; j++) {
+        if (nextIdx + j > numberOfDaysInMonth) {
+          week.push('hello');
+        } else {
+          week.push(nextIdx + j);
+        }
+      }
+
+      weeks.push(week);
+      nextIdx += 7
+    };
+
+    return weeks;
+  };
+
+  const nextIdxAfterFirstWeek = getNextStartIdx(firstWeek);
+  const remainingWeeks = generateRemainingWeeks(nextIdxAfterFirstWeek);
+  const weeksArray = [firstWeek].concat(remainingWeeks);
+
   return (
-    <>
-      <EmptyCells count={startIdx} />
-      <Days count={numberOfDaysInMonth} />
-      {remainingCellCount > 0 && <EmptyCells count={remainingCellCount} />}
-    </>
+    <div>
+      {weeksArray.map((week, weekIdx) => (
+        <Week key={weekIdx}>
+          {week.map((day, dayIdx) => (
+            <Day key={dayIdx}>{day}</Day>
+          ))}
+        </Week>
+      ))}
+    </div>
   );
 };
 
@@ -50,15 +86,25 @@ const Calendar = ({
   startIdx,
 }) => (
   <Grid>
-    <Header
-      date={date}
-      handleNavigationClick={handleNavigationClick}
-    />
-    <DaysOfWeek />
-    <Cells
-      numberOfDaysInMonth={numberOfDaysInMonth}
-      startIdx={startIdx}
-    />
+    <div>
+      <Header>
+        <Navigation
+          icon={'<'}
+          onClick={() => handleNavigationClick('back')}
+        />
+        <div>{date.format('MMMM')}</div>
+        <div>{date.format('YYYY')}</div>
+        <Navigation
+          icon={'>'}
+          onClick={() => handleNavigationClick('forward')}
+        />
+      </Header>
+      <DaysOfWeek />
+      <Weeks
+        numberOfDaysInMonth={numberOfDaysInMonth}
+        startIdx={startIdx}
+      />
+    </div>
   </Grid>
 );
 
@@ -100,43 +146,15 @@ function App() {
     .day();
 
   return (
-    <Container>
+    <AppContainer>
       <Calendar
         date={currentDate}
         handleNavigationClick={handleNavigationClick}
         startIdx={startIdx}
         numberOfDaysInMonth={numberOfDaysInMonth}
       />
-    </Container>
+    </AppContainer>
   );
 }
 
 export default App;
-
-const Container = styled.div`
-  padding: 36px;
-`;
-
-const Grid = styled.div`
-  display: grid;
-  grid-template-columns: repeat(7, 1fr);
-  grid-template-rows: repeat(7, 1fr);
-  border: 1px solid red;
-`;
-
-const HeaderContainer = styled.div`
-  display: grid;
-  grid-auto-flow: column;
-  grid-column-start: 1;
-  grid-column-end: 8;
-  justify-content: space-between;
-  align-items: center;
-  margin: 0 12px;
-`;
-
-const Cell = styled.div`
-  padding: 24px;
-  border: 1px solid;
-  background: white;
-  text-align: left;
-`;
