590db217c0a38e35ed78d54c6cc2f3efbc9d7547
Jexeones24 <jexeones@gmail.com>
2019-08-24 17:26:47 -0400

    adds tooltip

--- src/components/calendar/index.js
+++ src/components/calendar/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import moment from 'moment';
 import { generateWeeksArray } from './helpers';
 import {
@@ -9,51 +9,20 @@ import {
   Number,
   NumberWrapper,
   StyledNavigation,
+  TooltipWrapper,
   Week,
   Weekday,
   WeekdayHeader,
 } from './styles';
 
+import Form from '../form';
 import { Button } from '../shared/button';
 
 import { getDailyEvents } from '../../helpers';
 
 const WEEKDAY_SHORTNAMES = ['SUN', 'MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT'];
 
-const Header = ({ clickNavigationHandler, clickTodayHandler, date }) => (
-  <HeaderWrapper>
-    <Navigation icon={'<'} onClick={() => clickNavigationHandler('back')} />
-    <Button onClick={() => clickTodayHandler()}>Today</Button>
-    <div>{date.format('MMMM')}</div>
-    <div>{date.format('YYYY')}</div>
-    <Button>Month</Button>
-    <Navigation icon={'>'} onClick={() => clickNavigationHandler('forward')} />
-  </HeaderWrapper>
-);
-
-const Navigation = ({ icon, onClick }) => (
-  <StyledNavigation type='button' onClick={onClick}>
-    {icon}
-  </StyledNavigation>
-);
-
-const DaysOfWeek = () => (
-  <WeekdayHeader>
-    {WEEKDAY_SHORTNAMES.map((day, i) => (
-      <Weekday key={i}>{day}</Weekday>
-    ))}
-  </WeekdayHeader>
-);
-
-const renderNumber = (day, today) => day && (
-  <NumberWrapper>
-    <Number today={today}>
-      {day}
-    </Number>
-  </NumberWrapper>
-);
-
-const renderEvents = events => !!events.length &&
+const Events = events => !!events.length &&
   events.map(({ description, title }) => {
     const copy = `${title}: ${description}`;
     return (
@@ -63,55 +32,13 @@ const renderEvents = events => !!events.length &&
     )}
   );
 
+const Tooltip = ({ id, isOpen }) => id && (isOpen === id) && (
+  <TooltipWrapper>
+    <Form />
+  </TooltipWrapper>
+);
 
 const noop = () => ({});
-const DayContainer = ({ clickScheduleEventHandler, day, date, scheduledEvents }) => {
-  const today = date.date() === day && moment().isSame(date, 'month');
-  const year = date.year();
-  const month = date.month();
-  const formatMonth = String(month).length > 1 ? month : `0${month}`;
-  const formatDay = String(day).length > 1 ? day : `0${day}`;
-
-  const dateStr = `${year}-${formatMonth}-${formatDay}`;
-  const formatDate = moment(dateStr, 'YYYY-MM-DD').add(1, 'month')
-
-  const events = getDailyEvents(scheduledEvents, formatDate);
-
-  return (
-    <Day onClick={() => { day ?  clickScheduleEventHandler(day) : noop()}}>
-      {renderNumber(day, today)}
-      {renderEvents(events)}
-    </Day>
-  );
-};
-
-const Weeks = ({
-  date,
-  clickScheduleEventHandler,
-  numberOfDaysInMonth,
-  scheduledEvents,
-  startIdx,
-}) => {
-  const weeksArray = generateWeeksArray(numberOfDaysInMonth, startIdx)
-
-  return (
-    <div>
-      {weeksArray.map((week, weekIdx) => (
-        <Week key={weekIdx}>
-          {week.map((day, dayIdx) => (
-            <DayContainer
-              clickScheduleEventHandler={clickScheduleEventHandler}
-              day={day}
-              date={date}
-              key={dayIdx}
-              scheduledEvents={scheduledEvents}
-            />
-          ))}
-        </Week>
-      ))}
-    </div>
-  );
-};
 
 const Calendar = ({
   clickNavigationHandler,
@@ -121,24 +48,73 @@ const Calendar = ({
   numberOfDaysInMonth,
   scheduledEvents,
   startIdx,
-}) => (
-  <Grid>
-    <div>
-      <Header
-        clickNavigationHandler={clickNavigationHandler}
-        clickTodayHandler={clickTodayHandler}
-        date={date}
-      />
-      <DaysOfWeek />
-      <Weeks
-        clickScheduleEventHandler={clickScheduleEventHandler}
-        date={date}
-        numberOfDaysInMonth={numberOfDaysInMonth}
-        scheduledEvents={scheduledEvents}
-        startIdx={startIdx}
-      />
-    </div>
-  </Grid>
-);
+}) => {
+  const [clickedOn, setClickedOn] = useState(null);
+
+  const getToday = day => date.date() === day && moment().isSame(date, 'month');
+  const getEvents = day => {
+    const year = date.year();
+    const month = date.month();
+    const formatMonth = String(month).length > 1 ? month : `0${month}`;
+    const formatDay = String(day).length > 1 ? day : `0${day}`;
+
+    const dateStr = `${year}-${formatMonth}-${formatDay}`;
+    const formatDate = moment(dateStr, 'YYYY-MM-DD').add(1, 'month')
+
+    return getDailyEvents(scheduledEvents, formatDate);
+  };
+
+  const handleSchedulingClick = day => {
+    setClickedOn(day);
+  };
+
+  return (
+    <Grid>
+      <div>
+        <HeaderWrapper>
+          <StyledNavigation
+            type='button'
+            onClick={() => clickNavigationHandler('back')}>
+            {'<'}
+          </StyledNavigation>
+          <Button onClick={() => clickTodayHandler()}>Today</Button>
+          <div>{date.format('MMMM')}</div>
+          <div>{date.format('YYYY')}</div>
+          <Button>Month</Button>
+          <StyledNavigation
+            type='button'
+            onClick={() => clickNavigationHandler('forward')}>
+            {'>'}
+          </StyledNavigation>
+        </HeaderWrapper>
+        <WeekdayHeader>
+          {WEEKDAY_SHORTNAMES.map((day, i) => (
+            <Weekday key={i}>{day}</Weekday>
+          ))}
+        </WeekdayHeader>
+        {generateWeeksArray(numberOfDaysInMonth, startIdx).map((week, weekIdx) => (
+          <Week key={weekIdx}>
+            {week.map((day, dayIdx) => (
+              <Day key={dayIdx} onClick={() => { day ?  handleSchedulingClick(day) : noop()}}>
+                {day && (
+                  <NumberWrapper>
+                    <Number today={getToday(day)}>
+                      {day}
+                    </Number>
+                  </NumberWrapper>
+                )}
+                <Events events={getEvents(day)} />
+                <Tooltip
+                  id={day}
+                  isOpen={clickedOn}
+                />
+              </Day>
+            ))}
+          </Week>
+        ))}
+      </div>
+    </Grid>
+  );
+};
 
 export default Calendar;
